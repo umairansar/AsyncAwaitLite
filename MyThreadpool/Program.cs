e0f5dafbc8de178ba15aa128d7a1747ee265a8a3
muhammad.a2 <muhammad.a@snoonu.com>
2025-04-07 13:20:23 +0300

    Add project ThreadPool

---
+++ MyThreadpool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+
+for (int i = 0; i < 100; i++)
+{
+    int currentContext_i = i;
+    MyThreadPool.QueueUserWorkItem(delegate
+    {
+        Console.WriteLine(currentContext_i);
+        Thread.Sleep(1000);
+    });
+}
+
+Console.ReadLine();
+
+static class MyThreadPool
+{
+    private static readonly BlockingCollection<Action> s_workItems = new();
+
+    public static void QueueUserWorkItem(Action action) => s_workItems.Add(action);
+
+    static MyThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Action workItem = s_workItems.Take();
+                    workItem();
+                }
+            })
+            {
+                IsBackground = true
+            }.Start();
+        }
+    }
+}
